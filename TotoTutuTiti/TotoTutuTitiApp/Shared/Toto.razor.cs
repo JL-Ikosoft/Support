e2531692443ab92321757502984292594d00f54c
Julien Lefevre <julien.lefevre@ikosoft.cloud>
2022-06-28 22:37:50 +0200

    Sample app to demonstrate the issue

---
+++ TotoTutuTiti/TotoTutuTitiApp/Shared/Toto.razor.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorApp.Shared
+{
+    public partial class Toto : IAsyncDisposable
+    {
+        [Parameter]
+        public string? Tab { get; set; }
+
+        [Parameter]
+        public RenderFragment? ChildContent { get; set; }
+
+        public async ValueTask DisposeAsync()
+        {
+            await ValueTask.CompletedTask;
+        }
+    }
+}
